c8065cd1e79427b8d36a555ce1c8387aa39d7517
sk <jsckhan@outlook.com>
2023-01-31 18:10:32 -0500

    task: remove template files from intialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="ml-3">
+        <p className="text-sm font-medium text-gray-900">My Team</p>
+        <p className="text-sm text-gray-500">Kitchener</p>
+      </div>
     </div>
   );
 }
