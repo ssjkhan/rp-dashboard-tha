32658d3557980b6422ac7b5fec493e8c48926aa8
sk <jsckhan@outlook.com>
2023-01-31 20:00:09 -0500

    feat: daisyui components and base css for project

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
         <p className="text-sm font-medium text-gray-900">My Team</p>
         <p className="text-sm text-gray-500">Kitchener</p>
       </div>
+      <div className="btn">
+        This is a test button
+      </div>
     </div>
   );
 }
