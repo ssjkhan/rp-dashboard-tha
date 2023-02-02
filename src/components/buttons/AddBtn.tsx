8c2cd8e99949ab938539fbab4fd1498131b9bc09
sk <jsckhan@outlook.com>
2023-02-02 06:08:35 -0500

    feat: styled button components

---
+++ src/components/buttons/AddBtn.tsx
@@ -0,0 +1,20 @@
+function AddBtn() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-4 lg:w-6 h-4 lg:h-6 flex-no-shrink fill-lime-600"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+}
+
+export default AddBtn;
---
+++ src/components/buttons/RemoveBtn.tsx
@@ -0,0 +1,20 @@
+function RemoveBtn() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-4 lg:w-6 h-4 lg:h-6 fill-slate-900 text-white"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+}
+
+export default RemoveBtn;
